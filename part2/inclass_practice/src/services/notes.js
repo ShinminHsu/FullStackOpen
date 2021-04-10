064d93fe0e45f304825ef24e8d6b813069afae9c
Shinmin Hsu <shinminhsu@gmail.com>
2021-04-10 23:48:37 +0800

    Finish inclass practice in part2/d

---
+++ part2/inclass_practice/src/services/notes.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/notes'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
+const create = newObject => {
+  const request = axios.post(baseUrl, newObject)
+  return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then(response => response.data)
+}
+
+// since the names of the keys and the assigned variables are the same, we can simply the object name only instead of in the form of key: value
+export default { getAll, create, update }
